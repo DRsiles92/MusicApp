e6da526226559fa394ec8e73728580af5a2a7649
Greg Roberts <groberts3104@gmail.com>
2019-11-23 13:33:22 -0500

    added more to script

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+var searchBtnEl = $("#searchBtn");
 
-$.ajax({
-    url: "https://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay",
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
 
+
+$(searchBtnEl).click(function () {
+    var inputVal = $("#inputInfo").val();
+    var addDiv = $("<div>", {
+        'class': "searchObject"
+    });
+    $.ajax({
+        url: "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" + inputVal,
+        method: "GET"
+    }).then(function (response) {
+        $(addDiv).appendTo(".results");
+        $(".searchObject").text(response);
+        console.log(response);
+    })
+})
